14ea2094befd1fae2c63b2cb5378ca6d433e96b0
Andriy <andriy.andriyan1@gmail.com>
2023-01-04 10:38:27 +0700

    Add params to search category

--- app/Controllers/Http/ArticleCategoriesController.ts
+++ app/Controllers/Http/ArticleCategoriesController.ts
@@ -3,9 +3,17 @@ import ArticleCategory from 'App/Models/ArticleCategory';
 
 export default class ArticleCategoriesController {
   public async index({ request }: HttpContextContract) {
+    const search = request.input('search', '');
     const page = request.input('page', 1);
     const perPage = request.input('perPage', 20);
-    const data = await ArticleCategory.query().orderBy('name', 'asc').paginate(page, perPage);
+    const data = await ArticleCategory.query()
+      .where(query => {
+        if (search) {
+          query.whereILike('name', `%${search}%`);
+        }
+      })
+      .orderBy('name', 'asc')
+      .paginate(page, perPage);
     return data;
   }
 }
