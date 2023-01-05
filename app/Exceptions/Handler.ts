7c8721d12b80dfbb4184defff95f85763d8a5c02
Andriy <andriy.andriyan1@gmail.com>
2023-01-05 07:47:38 +0700

    add telegram error bot

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -13,11 +13,28 @@
 |
 */
 
+import Env from '@ioc:Adonis/Core/Env';
 import Logger from '@ioc:Adonis/Core/Logger';
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler';
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import TelegramBot from 'node-telegram-bot-api';
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   constructor() {
     super(Logger);
+  }
+
+  public async report(error: any, ctx: HttpContextContract) {
+    if (!this.shouldReport(error)) {
+      return;
+    }
+
+    if (typeof error.report === 'function') {
+      error.report(error, ctx);
+      return;
+    }
+    const token = Env.get('TELEGRAM_ERROR_BOT_TOKEN');
+    const bot = new TelegramBot(token);
+    await bot.sendMessage(Env.get('TELEGRAM_CHAT_ID'), error.stack);
   }
 }
