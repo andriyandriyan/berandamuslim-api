75bfc3d0ec0547a12df18738934e3ea8a997481e
Andriy <andriy.andriyan1@gmail.com>
2023-01-24 21:28:55 +0700

    chores: remove ScrapersController

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,39 +1,3 @@
-export interface Post {
-  id: number;
-  date: string;
-  modified: string;
-  link: string;
-  title: Title;
-  _embedded?: Embedded;
-}
-
-export interface Embedded {
-  'wp:featuredmedia': WpFeaturedmedia[];
-  'wp:term': WpTerm[][];
-}
-
-export interface WpFeaturedmedia {
-  id: number;
-  date: string;
-  slug: string;
-  link: string;
-  title: Title;
-  caption: Title;
-  alt_text: string;
-  source_url: string;
-}
-
-export interface WpTerm {
-  id: number;
-  name: string;
-  slug: string;
-  taxonomy: 'category' | 'post_tag';
-}
-
-export interface Title {
-  rendered: string;
-}
-
 export interface SholatScheduleResponse {
   status: boolean;
   data: SholatSchedule;
